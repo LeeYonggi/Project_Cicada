f09a5e8fb7b5112dde3bdd3591dca9b036c460da
BroYee <qqas1211yj@gmail.com>
2018-10-27 15:19:49 +0900

    Stage Select added

---
+++ Cicada/Assets/Broyee/StageSelect/StageButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageButton : MonoBehaviour {
+
+    public bool locked;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClick()
+    {
+        if (!locked)
+        {
+            MovePlayerToCurrentStage();
+        }
+    }
+
+    void MovePlayerToCurrentStage()
+    {
+        transform.parent.Find("Player").position = transform.position;
+    }
+
+}
