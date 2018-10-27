17a6efc379fcc0224021ef8704f4fffbfc3f01e8
BroYee <qqas1211yj@gmail.com>
2018-10-27 17:21:37 +0900

    MainMenu and Stage Clear added

---
+++ Cicada/Assets/Broyee/StageSelect/GoToStageSelect.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GoToStageSelect : MonoBehaviour {
+
+    public void OnClick()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+}
