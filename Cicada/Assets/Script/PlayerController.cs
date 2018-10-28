b543817281e9791e1823d7b13aeb03f2a78b0fbe
LeeYonggi <35089435+LeeYonggi@users.noreply.github.com>
2018-10-29 01:28:27 +0900

    다양한거, 공격추가해야됨

--- Cicada/Assets/Script/PlayerController.cs
+++ Cicada/Assets/Script/PlayerController.cs
@@ -18,26 +18,35 @@ public class PlayerController : PhysicsObject{
 
     private SpriteRenderer spriteRenderer;
     private Animator m_Animator;
+    private Rigidbody2D rb;
 
     Vector2 directionX;
-    Rigidbody2D rb;
-    Vector2 move = Vector2.zero;
-    
+    Vector2 move;
+
 
     // Use this for initialization
     void Start () {
         spriteRenderer = GetComponent<SpriteRenderer>();
         m_Animator = GetComponent<Animator>();
-	}
+        rb = GetComponent<Rigidbody2D>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        move = Vector2.zero;
-        move.x = Input.GetAxis("Horizontal");
+        Vector2 horizontal = Vector2.zero;
+        horizontal.x = Input.GetAxis("Horizontal");
+        if (horizontal.x != 0.0f)
+        {
+            move = horizontal;
+            MovePlayer();
+            move = Vector2.zero;
+        }
+        else
+            MovePlayer();
 
-        SetDirection();
-
-        if (Input.GetButtonDown("Jump") && grounded){
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
             velocity.y = jumpTakeOffSpeed;
             isJump = true;
         }
@@ -48,52 +57,47 @@ public class PlayerController : PhysicsObject{
             }
         }
 
-        if(move.x > 0.01f)
+        m_Animator.SetBool("isJump", isJump);
+        m_Animator.SetBool("grounded", grounded);
+        m_Animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+        
+	}
+
+    void JumpPlayer()
+    {
+        rb.AddForce(Vector2.up * jumpTakeOffSpeed, ForceMode2D.Impulse);
+        isJump = true;
+    }
+
+    void MovePlayer()
+    {
+        if (move.x > 0.01f)
         {
-            if(spriteRenderer.flipX == true)
+            if (spriteRenderer.flipX == true)
             {
                 spriteRenderer.flipX = false;
             }
         }
-        else if(move.x < -0.01f)
+        else if (move.x < -0.01f)
         {
-            if(spriteRenderer.flipX ==false)
+            if (spriteRenderer.flipX == false)
             {
                 spriteRenderer.flipX = true;
             }
         }
-        m_Animator.SetBool("isJump", isJump);
-        m_Animator.SetBool("grounded", grounded);
-        m_Animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
         targetVelocity = move * maxSpeed;
-	}
-
-    void SetDirection()
-    {
-        if(moveDirection == DIRECTION.LEFT)
-        {
-            move = Vector2.left;
-        }
-        else if(moveDirection == DIRECTION.RIGHT)
-        {
-            move = Vector2.right;
-        }
-        else
-        {
-            move = Vector2.zero;
-        }
     }
 
     public void MoveLeft()
     {
-        moveDirection = DIRECTION.LEFT;
+        move = Vector2.left * 0.7f;
     }
     public void MoveRight()
     {
-        moveDirection = DIRECTION.RIGHT;
+        move = Vector2.right * 0.7f;
     }
     public void StopMove()
     {
-        moveDirection = DIRECTION.NONE;
+        move = Vector2.zero;
     }
 }
