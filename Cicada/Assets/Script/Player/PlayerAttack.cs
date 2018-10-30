067e372aa101c29d3eef381c6beff9429e7c4ba1
LeeYonggi <rl4459@naver.com>
2018-10-30 12:05:03 +0900

    Add AttackScript

--- Cicada/Assets/Script/Player/PlayerAttack.cs
+++ Cicada/Assets/Script/Player/PlayerAttack.cs
@@ -4,19 +4,42 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour {
 
+    private float destroyDelay;
+    private GameObject player;
 
-	// Use this for initialization
-	void Start () {
+    public void AttackInit(float _destroyDelay, GameObject _player)
+    {
+        destroyDelay = _destroyDelay;
+        player = _player;
+    }
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (destroyDelay < 0)
+            Destroy(gameObject);
+        else
+            destroyDelay -= Time.deltaTime;
+
+        if(player.GetComponent<SpriteRenderer>().flipX == true)
+        {
+            transform.position = new Vector3(player.transform.position.x - 0.7f, player.transform.position.y);
+        }
+        else
+        {
+            transform.position = new Vector3(player.transform.position.x + 0.7f, player.transform.position.y);
+        }
 	}
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.gameObject.tag == "Monster")
+        {
+            
+        }
     }
 }
