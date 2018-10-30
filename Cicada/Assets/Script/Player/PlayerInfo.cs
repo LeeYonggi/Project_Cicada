554c5ae707c91336e85c667f2e1fb91ff967138c
LeeYonggi <rl4459@naver.com>
2018-10-30 21:06:26 +0900

    Add Attacked

---
+++ Cicada/Assets/Script/Player/PlayerInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour {
+
+    [SerializeField]
+    private int hp;
+    private int attack;
+
+    public int Attack
+    {
+        get
+        {
+            return attack;
+        }
+
+        set
+        {
+            attack = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AddAttacked(int damage)
+    {
+        hp -= damage;
+    }
+}
