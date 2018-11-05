bd077269500d2b26f3d16e60e7d83c2666f4a4c1
BroYee <qqas1211yj@gmail.com>
2018-11-05 16:44:27 +0900

    Xml Serializer almost done

---
+++ Cicada/Assets/Broyee/StageSelect/StageGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageGenerator : MonoBehaviour {
+
+    public int map;
+    public int stage;
+
+    private void Start()
+    {
+        GenerateStage();
+    }
+
+    public void GenerateStage()
+    {
+        string levelName = map.ToString() + "_" + stage.ToString();
+
+        var mapData = MapData.Load("Assets/Resource/Xml/" + levelName + ".xml");
+
+        //GameObject.FindGameObjectWithTag("Player").gameObject.transform.position = mapData.playerPos;
+        Debug.Log("Instantiate!!");
+        Instantiate(Resources.Load("Prefabs/LevelDesigns/" + levelName + "/" + levelName + "Map"));
+        for (int i = 0; i < mapData.monsters.Length; i++)
+        {
+            Debug.Log(mapData.monsters[i].name);
+            Instantiate(Resources.Load("Prefabs/Monsters/" + mapData.monsters[i].name) as GameObject, mapData.monsters[i].pos, Quaternion.identity);
+        }
+    }
+}
--- Cicada/Assets/Broyee/StageSelect/StageStart.cs
+++ Cicada/Assets/Broyee/StageSelect/StageStart.cs
@@ -13,24 +13,13 @@ public class StageStart : MonoBehaviour {
 
     private void Awake()
     {
-        DontDestroyOnLoad(gameObject);
+        //DontDestroyOnLoad(gameObject);
     }
 
     public void StartStage()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(2);
 
-        string levelName = map.ToString() + "_" + stage.ToString();
-
-        var mapData = MapData.Load("Assets/Resource/Xml/" + levelName + ".xml");
-
-        //GameObject.FindGameObjectWithTag("Player").gameObject.transform.position = mapData.playerPos;
-        Debug.Log("Instantiate!!");
-        Instantiate(Resources.Load("Prefabs/LevelDesigns/" + levelName + "/" + levelName + "Map") as GameObject, new Vector3(0, 0, 0), Quaternion.identity);
-        for (int i = 0; i < mapData.monsters.Length; i++)
-        {
-            Debug.Log(mapData.monsters[i].name);
-            Instantiate(Resources.Load("Prefabs/Monsters/" + mapData.monsters[i].name) as GameObject, mapData.monsters[i].pos, Quaternion.identity);
-        }
+        GameObject.Find("StageGenerator").GetComponent<StageGenerator>().GenerateStage();
     }
 }
