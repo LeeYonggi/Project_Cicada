15e1740c4ecb3d24c4f30311083e8e6afd75de28
BroYee <qqas1211yj@gmail.com>
2018-11-06 23:04:50 +0900

    TouchToStart added

--- Cicada/Assets/Broyee/MainMenu/TouchToStart.cs
+++ Cicada/Assets/Broyee/MainMenu/TouchToStart.cs
@@ -10,19 +10,27 @@ public class TouchToStart : MonoBehaviour {
 	void Start () {
         image = GetComponent<UnityEngine.UI.Image>();
 
-
-	}
+        StartCoroutine(Blink());
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Time.timeScale = 1;
+            UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+        }
 	}
 
     private IEnumerator Blink()
     {
-        while (true)
-        {
-            //image.color = Color.
-        }
+        image.color = new Color(1, 1, 1, 0.5f);
+        yield return new WaitForSeconds(0.3f);
+
+        image.color = new Color(1, 1, 1, 1);
+        yield return new WaitForSeconds(0.3f);
+
+        StartCoroutine(Blink());
     }
 }
