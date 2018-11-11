dc60f158ed15188dbaa2ca42f9a87d9f8ffe4039
BroYee <qqas1211yj@gmail.com>
2018-11-12 02:01:35 +0900

    Made monsters sounds distant

--- Cicada/Assets/Broyee/Monsters/Plant/Pea.cs
+++ Cicada/Assets/Broyee/Monsters/Plant/Pea.cs
@@ -5,25 +5,36 @@ using UnityEngine;
 public class Pea : MonoBehaviour {
 
     public int damage;
-
     public float speed;
+
+    private bool popped;
 
 	// Use this for initialization
 	void Start () {
         GetComponent<Rigidbody2D>().AddForce(transform.up * speed * Time.deltaTime, ForceMode2D.Impulse);
+
+        popped = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        if (popped && !GetComponent<AudioSource>().isPlaying)
+        {
+            Destroy(gameObject);
+        }
 	}
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.CompareTag("Player") || col.CompareTag("Ground") || col.CompareTag("Wall"))
+        if (!popped)
         {
-            GetComponent<AudioSource>().Play();
-            Destroy(gameObject);
+            if (col.CompareTag("Player") || col.CompareTag("Ground") || col.CompareTag("Wall"))
+            {
+                GetComponent<AudioSource>().Play();                
+                popped = true;
+                GetComponent<SpriteRenderer>().enabled = false;
+            }
         }
     }
 }
