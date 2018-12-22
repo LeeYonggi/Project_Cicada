afcabb4b6bfc763bdc982f31af27721d2ed16bbe
BroYee <qqas1211yj@gmail.com>
2018-12-22 18:11:22 +0900

    Make spotlight look bliur

---
+++ Cicada/Assets/Broyee/Light/MaterialFlip.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialFlip : MonoBehaviour {
+
+    private SpriteRenderer spriteRenderer;
+
+    public Material leftMat;
+    public Material rightMat;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (spriteRenderer.flipX) spriteRenderer.material = leftMat;
+        else spriteRenderer.material = rightMat;
+    }
+}
