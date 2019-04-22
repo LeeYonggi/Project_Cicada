031628fbfb312d66343c4e976cd894353784e265
BroYee <qqas1211yj@gmail.com>
2019-04-22 19:20:34 +0900

    CreditButton added

---
+++ Cicada/Assets/Broyee/StageSelect/CreditButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditButton : MonoBehaviour {
+
+
+
+    public void EnableCreditScreen()
+    {
+        transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (transform.GetChild(0).gameObject.activeSelf && Input.anyKeyDown) transform.GetChild(0).gameObject.SetActive(false);
+    }
+}
